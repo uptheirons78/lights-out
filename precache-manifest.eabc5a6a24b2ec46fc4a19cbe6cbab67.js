9fcca05515066e5f71354014681e20f3cbcd9fe0
Mauro Bono <bono.mauro@gmail.com>
2019-06-14 13:30:28 +0200

    Updates

---
+++ precache-manifest.eabc5a6a24b2ec46fc4a19cbe6cbab67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a683fa89b76cdb2fbc83",
+    "url": "/lights-out/static/css/main.760abd71.chunk.css"
+  },
+  {
+    "revision": "a683fa89b76cdb2fbc83",
+    "url": "/lights-out/static/js/main.a683fa89.chunk.js"
+  },
+  {
+    "revision": "207e5d056e3105dcde38",
+    "url": "/lights-out/static/js/1.207e5d05.chunk.js"
+  },
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "33ccae521597d6292d58d05c6ac12f68",
+    "url": "/lights-out/index.html"
+  }
+];
